ff7de4a7500a7e30743793e79b7892ccf8655093
Galina <krasnikova.82@mail.ru>
2020-12-27 20:28:02 +0300

    первое задание

--- src/com/task1/Main.java
+++ src/com/task1/Main.java
@@ -1,8 +1,72 @@
 package com.task1;
 
-public class Main {
+//Заставить программу ”Hello, World!” упасть…
+     //   ● Смоделировав ошибку «NullPointerException»
+    //    ● Смоделировав ошибку «ArrayIndexOutOfBoundsException»
+    //    ● Вызвав свой вариант ошибки через оператор throw
 
-    public static void main(String[] args) {
-	// write your code here
+import java.util.Arrays;
+
+class HelloWorld {
+      String Hello;
+      String World;
+      public void Test (HelloWorld x){
+          x.Hello="Hello";
+      }
+}
+
+class ArrayIndex {
+    int[] myArray = {1, 2, 3, 4, 5};
+
+    public void DisPlay(int n) {
+        myArray[n] = 8;
     }
 }
+
+class MyException {
+    String name = "кошка";
+
+    public void Cat(String k) throws CatException {
+        if (k == name)
+            System.out.println(" любимое животное");
+        else throw new CatException ();
+
+    }
+}
+class CatException extends Exception {
+
+
+}
+
+
+
+class Main {
+    public static void main(String[] args) {
+        try {                                       //выполняется код
+            int g;
+            System.out.println("Hello, World!");
+            HelloWorld n = new HelloWorld();
+            n.Test(null);
+        } catch (NullPointerException I) {
+            System.out.println("Не обьявлен класс HelloWorld ");
+        }
+        try {
+            ArrayIndex D = new ArrayIndex();
+            D.DisPlay(7);
+        } catch (ArrayIndexOutOfBoundsException O) {
+            System.out.println("Ошибка при обращении к элементу массива по отрицательному или превышающему размер массива индексу");
+
+        }
+        try {
+            MyException S = new MyException();
+            S.Cat("собака");
+
+
+        } catch (CatException W) {
+            System.out.println("Введено не правильное имя домашнего животного, получили ошибку");
+        }
+    }
+}
+
+
+
